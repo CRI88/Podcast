b2487e9b8c818961ad3c8e14b0474d235ab72148
toniramss <toni.rams.sebastia@gmail.com>
2025-03-25 18:18:23 +0100

    eliminar index.php y actualizar episodios.php para redirigir botones de inicio de sesión y registro a index.php

--- episodios.php
+++ episodios.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,10 @@
     <link rel="icon" href="img/logo.webp" type="image/png">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <script src="functions/modal.js"></script>
-    <script type="module" src="functions/episodios.js" defer></script>
+    <script type="module" src="functions/index.js"></script>
 </head>
-<body id="contenedorBodyEpisodios">
+
+<body>
     <?php include 'header.php'; ?>
 
     <!-- Modal de Inicio de Sesión / Registro -->
@@ -25,7 +27,8 @@
                 <h2 class="text-xl font-bold text-center mb-4">Iniciar Sesión</h2>
                 <input type="email" placeholder="Correo electrónico" class="w-full border p-2 mb-2 rounded-lg">
                 <input type="password" placeholder="Contraseña" class="w-full border p-2 mb-4 rounded-lg">
-                <button class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-700" onclick="window.location.href='episodios.php'">Ingresar</button>
+                <button class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-700"
+                    onclick="window.location.href='index.php'">Ingresar</button>
                 <p class="text-sm text-center mt-4">
                     ¿No tienes cuenta?
                     <a href="#" class="text-blue-500 hover:underline" onclick="showRegister()">Regístrate</a>
@@ -38,7 +41,8 @@
                 <input type="text" placeholder="Nombre" class="w-full border p-2 mb-2 rounded-lg">
                 <input type="email" placeholder="Correo electrónico" class="w-full border p-2 mb-2 rounded-lg">
                 <input type="password" placeholder="Contraseña" class="w-full border p-2 mb-4 rounded-lg">
-                <button class="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-700" onclick="window.location.href='episodios.php'">Registrarse</button>
+                <button class="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-700"
+                    onclick="window.location.href='index.php'">Registrarse</button>
                 <p class="text-sm text-center mt-4">
                     ¿Ya tienes cuenta?
                     <a href="#" class="text-blue-500 hover:underline" onclick="showLogin()">Iniciar sesión</a>
@@ -48,20 +52,24 @@
     </div>
 
 
+    <section>
+
+        <div class="p-10">
+            <div
+                class="w-full h-100 flex flex-col gap-4 p-10 justify-end rounded-lg bg-cover bg-center bg-no-repeat bg-[url('https://blog.freemusicprojects.com/wp-content/uploads/2019/10/music-production-banner.jpg')]">
+                <h1 class="text-white font-bold text-5xl">Reproduce tus pistas</h1>
+            </div>
+        </div>
 
 
+        <div id="contenedorHome" class="w-full flex flex-row flex-wrap items-center justify-center gap-8 px-10">
 
+        </div>
+    </section>
 
-    <h1 class="w-full text-3xl text-blue-500 text-center">Podcasts</h1>
-    <div id="divListaPodcasts" class="flex flex-col gap-4 p-10">
-    </div>
-
-
-
-
-
-
+    <br><br>
 
     <?php include 'footer.php'; ?>
 </body>
+
 </html>
